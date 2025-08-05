98b4160602da7d098f0b6f4f19971b696854812f
Anuj Singh <anuj1112131415@gmail.com>
2025-08-05 16:50:51 +0530

    Initialize JetpackApp1 Android project
    Add initial project structure for JetpackApp1, including Gradle build files, source code, resources, and configuration files. Implements a basic Jetpack Compose UI in MainActivity with theme setup, sample image, and buttons. Includes default .gitignore files, test classes, and AndroidManifest configuration.

---
+++ Jetpack Apps/Jetpack Practice/JetpackApp1/app/src/main/java/com/example/jetpackapp1/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.jetpackapp1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpackapp1.ui.theme.JetpackApp1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackApp1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Test()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun Test() {
+
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center)
+    {
+        Card (
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+        ) {
+            Column (
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Hello World", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Image(painter = painterResource(id = R.drawable.earth), contentDescription = "",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(100.dp).clip(RoundedCornerShape(10.dp)))
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = { /*TODO*/}
+                ) {
+                    Text(text = "Share")
+                }
+
+                Button(
+                    onClick = { /*TODO*/}
+                ) {
+                    Text(text = "Subscribe")
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Jetpack Apps/Jetpack Practice/JetpackApp1/app/src/main/java/com/example/jetpackapp1/state.kt
@@ -0,0 +1,9 @@
+package com.example.jetpackapp1
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun state() {
+
+}
