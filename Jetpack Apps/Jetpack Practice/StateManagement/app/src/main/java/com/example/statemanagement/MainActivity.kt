6d07b83a21b2dfd699d436d772ec1eb482e19ac4
Anuj Singh <anuj1112131415@gmail.com>
2025-08-19 11:28:35 +0530

    Initialize Jetpack Compose StateManagement app
    Add initial project structure for a Jetpack Compose Android app named StateManagement. Includes Gradle configuration, theme setup, main activity, resources, and basic unit/instrumented tests.

---
+++ Jetpack Apps/Jetpack Practice/StateManagement/app/src/main/java/com/example/statemanagement/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.statemanagement
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.statemanagement.ui.theme.StateManagementTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StateManagementTheme {
+                var textFieldValue by remember {
+                    mutableStateOf("")
+                }
+
+                var checkBoxValue by rememberSaveable {
+                    mutableStateOf(true)
+                }
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        TextField(value = textFieldValue, onValueChange = {
+                            textFieldValue = it
+                            println(it)
+                        })
+
+
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivity", "onRestart")
+    }
+
+    
+}
